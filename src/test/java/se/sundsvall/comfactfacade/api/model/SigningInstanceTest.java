32772e7567747d5f34147e4d59a8400d2955966c
Joakim Schürer <joakim.schurer@sundsvall.se>
2024-03-26 08:01:28 +0100

    UF-7411: Fixed failing tests

--- src/test/java/se/sundsvall/comfactfacade/api/model/SigningInstanceTest.java
+++ src/test/java/se/sundsvall/comfactfacade/api/model/SigningInstanceTest.java
@@ -5,13 +5,15 @@ import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static com.google.code.beanmatchers.BeanMatchers.registerValueGenerator;
+import static java.time.OffsetDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.allOf;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Random;
 
-import com.google.code.beanmatchers.BeanMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,7 +22,7 @@ class SigningInstanceTest {
 
 	@BeforeAll
 	static void setUp() {
-		BeanMatchers.registerValueGenerator(OffsetDateTime::now, OffsetDateTime.class);
+		registerValueGenerator(() -> now().plusDays(new Random().nextInt()), OffsetDateTime.class);
 	}
 
 	@Test
--- src/test/java/se/sundsvall/comfactfacade/api/model/SigningRequestTest.java
+++ src/test/java/se/sundsvall/comfactfacade/api/model/SigningRequestTest.java
@@ -5,14 +5,15 @@ import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static com.google.code.beanmatchers.BeanMatchers.registerValueGenerator;
 import static java.time.OffsetDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.allOf;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Random;
 
-import com.google.code.beanmatchers.BeanMatchers;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -21,7 +22,7 @@ class SigningRequestTest {
 
 	@BeforeAll
 	static void setUp() {
-		BeanMatchers.registerValueGenerator(OffsetDateTime::now, OffsetDateTime.class);
+		registerValueGenerator(() -> now().plusDays(new Random().nextInt()), OffsetDateTime.class);
 	}
 
 	@Test
