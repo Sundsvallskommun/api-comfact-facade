72ba650a70e72e9bb0b38ee3e558043d10bb667c
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:54:31 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/comfactfacade/api/SigningResourceFailureTest.java
+++ src/test/java/se/sundsvall/comfactfacade/api/SigningResourceFailureTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zalando.problem.Problem;
@@ -31,7 +31,7 @@ import se.sundsvall.comfactfacade.service.SigningService;
 @ActiveProfiles("junit")
 class SigningResourceFailureTest {
 
-	@MockBean
+	@MockitoBean
 	private SigningService signingServiceMock;
 
 	@Autowired
